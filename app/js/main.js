033e7e665592948533b28078984d9152814d954d
Yigal Igor Maksimov <yigal.maksimov@gmail.com>
2020-07-09 19:57:42 +0300

    added new product section and filer plugin

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,22 @@
-$(function (){
+$(function () {
+  $(".weekly__product").slick({
+    prevArrow:
+      '<button type="button" class="slick-btn slick-prev"> <img src="img/icons/arrow-left.png" alt="">',
+    nextArrow:
+      '<button type="button" class="slick-btn slick-next"> <img src="img/icons/arrow-right.png" alt="">',
+      
+  });
 
+  $(".weekly__product-info-rate").rateYo({
+    rating: 4,
+    readOnly: true,
+    starWidth: "20px",
+  });
+  $(".product__rate").rateYo({
+    rating: 4,
+    readOnly: true,
+    starWidth: "12px",
+  });
 
-  
-
+  var mixer = mixitup('.newproducts__items');
 });
--- gulpfile.js
+++ gulpfile.js
@@ -39,6 +39,8 @@ gulp.task("js", function () {
     .src([
       "node_modules/slick-carousel/slick/slick.js",
       // "node_modules/magnific-popup/dist/jquery.magnific-popup.js",
+      "node_modules/rateyo/src/jquery.rateyo.js",
+      "node_modules/mixitup/dist/mixitup.js",
     ])
     .pipe(concat("libs.min.js"))
     .pipe(uglify())
@@ -52,6 +54,7 @@ gulp.task("css", function () {
       "node_modules/normalize.css/normalize.css",
       "node_modules/slick-carousel/slick/slick.css",
       // "node_modules/magnific-popup/dist/magnific-popup.css",
+      "node_modules/rateyo/src/jquery.rateyo.css",
     ])
     .pipe(concat("_libs.scss"))
     .pipe(gulp.dest("app/scss"))
