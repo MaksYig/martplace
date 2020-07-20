199fb1d43114ce18f5a4350cfa018fa58df905c1
Yigal Igor Maksimov <yigal.maksimov@gmail.com>
2020-07-20 20:55:11 +0300

    added new item transforme and asside menu

--- app/js/main.js
+++ app/js/main.js
@@ -13,11 +13,32 @@ $(function () {
     slidesToScroll: 1,
   });
 
+  $(".icon-th-large").on("click", function () {
+    $(".product__items-list").addClass("large");
+    $(".icon-th-large").addClass("active");
+    $(".icon-th-list").removeClass("active");
+  });
+
+  $(".icon-th-list").on("click", function () {
+    $(".product__items-list").removeClass("large");
+    $(".icon-th-list").addClass("active");
+    $(".icon-th-large").removeClass("active");
+  });
+
+  $(".category__title").on("click", function () {
+    $(".asside__categories").toggleClass("close");
+  });
+  $(".filter__title").on("click", function () {
+    $(".asside__filter-products").toggleClass("close");
+  });
+  $(".range__title").on("click", function () {
+    $(".price-range__content").toggleClass("close");
+  });
+
   $(".header__burger").on("click", function () {
     $(".header__burger, .menu").toggleClass("active");
     $("body").toggleClass("lock");
   });
-
 
   $(".weekly__product-info-rate").rateYo({
     rating: 4,
@@ -30,14 +51,14 @@ $(function () {
     starWidth: "12px",
   });
   $(".product__item-rate").rateYo({
-    rating: 4,
+    // rating: 4,
     readOnly: true,
-    starWidth: "17px",
+    // starWidth: "17px",
   });
   $(".product__item-list-rate").rateYo({
-    rating: 4,
-    readOnly: true,
-    starWidth: "14px",
+    // rating: 4,
+    // readOnly: true,
+    // starWidth: "14px",
   });
 
   $(".js-range-slider").ionRangeSlider({
